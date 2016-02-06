fe83a366f0c8441c2c08ffd6335c17e24ea91b64
shigemk2 <i.am.shige@gmail.com>
2016-02-07 02:02:57 +0900

    Add

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,50 +1,10 @@
-import React from 'react'
-import Modal from 'react-modal';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-var appElement = document.getElementById('app-container');
-
-Modal.setAppElement(appElement);
-Modal.injectCSS();
-
-var App = React.createClass({
-
-    getInitialState: function() {
-        return { modalIsOpen: false };
-    },
-
-    openModal: function() {
-        this.setState({modalIsOpen: true});
-    },
-
-    closeModal: function() {
-        this.setState({modalIsOpen: false});
-    },
-
+var MyComponent = React.createClass({
     render: function() {
-        return (
-            <div>
-                <button onClick={this.openModal}>Open Modal</button>
-                <Modal
-                    isOpen={this.state.modalIsOpen}
-                    onRequestClose={this.closeModal}
-                >
-                    <h2>Hello</h2>
-                    <button onClick={this.closeModal}>close</button>
-                    <div>I am a modal</div>
-                    <form>
-                        <input />
-                        <button>tab navigation</button>
-                        <button>stays</button>
-                        <button>inside</button>
-                        <button>the modal</button>
-                    </form>
-                </Modal>
-            </div>
-        );
+        return <div>test test test</div>;
     }
 });
 
-React.render(
-    <App/>,
-    appElement
-);
+ReactDOM.render(<MyComponent />, document.getElementById("content"));
