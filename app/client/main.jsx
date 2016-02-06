5e7263e5588bbeeaa4bb647ebfa0b679d34bb8af
shigemk2 <i.am.shige@gmail.com>
2016-02-06 23:51:04 +0900

    Fix

--- app/client/main.jsx
+++ app/client/main.jsx
@@ -1,11 +1,50 @@
 import React from 'react'
+import Modal from 'react-modal';
 
-class Foo extends React.Component{
-    render(){
-        return <div>This is React component</div>
+var appElement = document.getElementById('app-container');
+
+Modal.setAppElement(appElement);
+Modal.injectCSS();
+
+var App = React.createClass({
+
+    getInitialState: function() {
+        return { modalIsOpen: false };
+    },
+
+    openModal: function() {
+        this.setState({modalIsOpen: true});
+    },
+
+    closeModal: function() {
+        this.setState({modalIsOpen: false});
+    },
+
+    render: function() {
+        return (
+            <div>
+                <button onClick={this.openModal}>Open Modal</button>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                >
+                    <h2>Hello</h2>
+                    <button onClick={this.closeModal}>close</button>
+                    <div>I am a modal</div>
+                    <form>
+                        <input />
+                        <button>tab navigation</button>
+                        <button>stays</button>
+                        <button>inside</button>
+                        <button>the modal</button>
+                    </form>
+                </Modal>
+            </div>
+        );
     }
-}
+});
 
-var container = document.querySelector("#container")
-
-    React.render(<Foo />, container)
+React.render(
+    <App/>,
+    appElement
+);
